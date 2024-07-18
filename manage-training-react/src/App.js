6aa4c0448a4d25a966aed303576ef3772b00b10f
Guilherme Lobato <guilherme.lobato09@gmail.com>
2024-07-18 16:04:30 -0300

    Update App.js

--- manage-training-react/src/App.js
+++ manage-training-react/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Meus Treinos da Academia</h1>
+        <h1>Treinos</h1>
       </header>
       <main>
         <WorkoutList />
